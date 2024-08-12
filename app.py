58ba99b4706ac572b6575df871b4ff61a640b534
Akbar Ali <m.akberali507@gmail.com>
2024-08-12 17:21:07 +0500

    AI Background Remover

---
+++ app.py
@@ -0,0 +1,46 @@
+# Import the Dependencies
+from transformers import pipeline
+import os
+
+# Function to remove the background from the image
+def AI_Background_Remover(
+        image_folder:str, 
+        image_name:str, 
+        result_image_folder:str, 
+        result_image_name:str
+        ):
+    
+    # Image path
+    image_path = os.path.join(image_folder,image_name)
+
+    # Use the transformer library pipline
+    pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True, device="cpu") # You can use "cuda" also.
+
+    # Load the image
+    pillow_mask = pipe(image_path, return_mask = True) # outputs a pillow mask
+
+    pillow_image = pipe(image_path) # applies mask on input and returns a pillow image
+
+    # Save the mask
+    pillow_mask.save("segmented_mask.png")
+
+    # Save the image
+    pillow_image.save(os.path.join(result_image_folder, result_image_name))
+
+# To Remove specific images background.
+AI_Background_Remover(
+        image_folder="Image_with_background", 
+        image_name="portrait-beautiful-fashion-woman-with-black-makeup-golden-manicure.jpg",
+        result_image_folder="Image_without_background",
+        result_image_name=f"portrait-beautiful-fashion-woman-with-black-makeup-golden-manicure_rm_bg.png"
+        )
+
+# To Remove all images background that are save on "Image_with_background" Folder
+# for image_name in os.listdir("Image_with_background"):
+#     AI_Background_Remover(
+#         image_folder="Image_with_background", 
+#         image_name=image_name,
+#         result_image_folder="Image_without_background",
+#         result_image_name=f"{image_name[:-4]}_rm_bg.png"
+#         )
+#     print(f"{image_name} Background Removed")
